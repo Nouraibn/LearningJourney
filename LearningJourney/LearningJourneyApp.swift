580cc0f812d47a845efa3b3a839152d5e86ca1b8
Nouraibn <Noura_alnassar@hotmail.com>
2024-10-21 15:23:59 +0300

    Initial Commit

---
+++ LearningJourney/LearningJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningJourneyApp.swift
+//  LearningJourney
+//
+//  Created by Noura on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct LearningJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
